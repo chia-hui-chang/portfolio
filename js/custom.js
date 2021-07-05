53c4f4cff6c5260881c31790aa022e1a5991184e
chia-hui-chang <75665552+chia-hui-chang@users.noreply.github.com>
2021-07-05 11:29:00 +0800

    0705

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDistance > 80){
+			$header.addClass("header--colored");
+		}else{
+			$header.removeClass("header--colored");
+		}
+	})
+})
